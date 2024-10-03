15741d6063512c3f313cc8e6f644cfe8d98f55a7
secretwink <peter.530@outlook.com>
2024-10-03 13:27:52 -0300

    scan by zipcode
    implement with zipcode get api
    and show map

--- lang/en/ping.php
+++ lang/en/ping.php
@@ -8,5 +8,6 @@ return [
     'my-tags' => 'My Comments',
     'location' => 'Tagged Location',
     'new-location' => 'Tag with Location (ruins the fun if you don\'t!)',
-    'edit-location' => 'Tag with Location'
+    'edit-location' => 'Tag with Location',
+    'code' => 'Zip Code'
 ];
